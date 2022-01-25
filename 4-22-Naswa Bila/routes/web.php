0ce6d83017f1faebc65d7ad19dee7b14cf5a39d3
naswabillazakia <naswabillazakia@gmail.com>
2022-01-25 07:51:49 +0700

    upload modul 2

--- 4-22-Naswa Bila/routes/web.php
+++ 4-22-Naswa Bila/routes/web.php
@@ -14,19 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view ('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about' , [
-    "nama" => "Naswa Bila Zakia",
-    "email" => "naswabillazakia@gmail.com",
-    "gambar" => "naswa.jpg"
+    return view ('about', [
+        "title" => "About",
+        "nama" => "Naswa Bila Zakia",
+        "email" => "naswabillazakia@gmail.com",
+        "gambar" => "naswa.jpg"
     ]);
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view ('gallery', [
+        "title" => "Gallery"
+    ]);
 });
-
-
