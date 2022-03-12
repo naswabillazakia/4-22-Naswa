afce54fabb1e3a015609b9c1b5680b9cb0daf320
naswabillazakia <naswabillazakia@gmail.com>
2022-03-12 21:12:46 +0700

    upload modul 6

--- 4-22-Naswa Bila/resources/views/layouts/app.blade.php
+++ 4-22-Naswa Bila/resources/views/layouts/app.blade.php
@@ -1,4 +1,52 @@
-<!doctype html>
+<!doctype html><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Dashboard</title>
+
+    @include('partials/css');
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <!-- HEADER MOBILE-->
+       @include('partials/header-mobile');
+        <!-- END HEADER MOBILE-->
+       
+
+        <!-- MENU SIDEBAR-->
+        @include('partials/slidebar');
+        <!-- END MENU SIDEBAR-->
+
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            @include('partials/header');
+            <!-- HEADER DESKTOP-->
+
+            <!-- MAIN CONTENT-->
+            @yield('content');
+            <!-- END MAIN CONTENT-->
+            <!-- END PAGE CONTAINER-->
+        </div>
+
+    </div>
+
+    @include('partials/script');
+
+</body>
+
+</html>
+<!-- end document-->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
